d5321f0039aabcedfc3739eec6aaa61626044a23
Martin Lovell <martin.lovell@yale.edu>
2024-09-16 07:51:51 -0400

    Display version information

--- wikidata-site/src/About.js
+++ wikidata-site/src/About.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const About = ({about, sparql}) =>
-    <>
+const About = ({about, sparql})  => {
+    let [latestVersion, setLatestVersion] = useState();
+    const basename = document.querySelector('base')?.getAttribute('href') ?? '/'
+    useEffect(()=>{
+        fetch(`${basename}/LATEST_VERSION`)
+        .then(response => response.text())
+        .then(data => {
+            setLatestVersion(data)
+        })
+        .catch(error => console.error(error));
+    }, [basename]);
+    return <>
         <h1>About</h1>
         <p>{about}</p>
-        {sparql && <><strong>SPARQL:</strong><pre>{sparql}</pre></>}
+        {sparql && <div><strong>SPARQL:</strong><pre>{sparql}</pre></div>}
+        {latestVersion && <div>Version: {latestVersion}</div>}
     </>;
+}
 
 export default About;
